ac4e385b22a433144569a8beceaa741637f702a4
Ahmed Sadaawi <EmbabyProgramming@gmail.com>
2022-12-09 11:09:22 +0200

    aws:eb in .circleci

--- udagram/udagram-api/bin/aws_eb.sh
+++ udagram/udagram-api/bin/aws_eb.sh
@@ -1,4 +1,4 @@
 sudo apt-get -y -qq updates
-sudo apt-get install python3-pip python-dev build-essentail
+sudo apt-get install python3-pip python-dev-is-python2 build-essentail
 sudo pip install --upgrate setuptools
 sudo pip install awsbcli --upgrate
