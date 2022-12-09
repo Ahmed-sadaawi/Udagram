cb67e5f1422f1d8403f55614ecc2b5bd06ab6dd7
Ahmed Sadaawi <EmbabyProgramming@gmail.com>
2022-12-09 11:18:55 +0200

    aws:eb in .circleci

--- udagram/udagram-api/bin/aws_eb.sh
+++ udagram/udagram-api/bin/aws_eb.sh
@@ -1,4 +1,11 @@
-sudo apt-get -y -qq updates
+sudo apt-get update
+sudo apt install python
+sudo apt-get -qq -y install python3-dev
+sudo apt-get install python3-setuptools
+curl https://bootstrap.pypa.io/get-pip.py | python3
+sudo apt-get -qq -y install python3-venv
+pip install pipx
+pipx install awsebcli
 sudo apt-get install python3-pip python-dev-is-python2 build-essentail
 sudo pip install --upgrate setuptools
 sudo pip install awsbcli --upgrate
