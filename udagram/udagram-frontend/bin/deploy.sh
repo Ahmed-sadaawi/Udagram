604b88e646bff6de66f78f1c27909034f8b57b91
Ahmed Sadaawi <EmbabyProgramming@gmail.com>
2022-12-10 06:28:40 +0200

    new bucket

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://bucket2093458/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://bucket2093458/
+aws s3 cp --recursive --acl public-read ./www s3://newbucket8123049/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://newbucket8123049/
